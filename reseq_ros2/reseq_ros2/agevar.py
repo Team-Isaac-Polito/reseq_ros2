564ea67c4fd103e4310f3efc0d3b1d9ae0acc832
GBergatto <gb.bergatto@gmail.com>
2023-05-24 09:00:46 +0200

    Implement kinematic and yaw feedback callback

--- reseq_ros2/reseq_ros2/agevar.py
+++ reseq_ros2/reseq_ros2/agevar.py
@@ -19,6 +19,9 @@ class Agevar(Node):
             self.config = yaml.load(f, Loader=SafeLoader)
             print(self.config)
 
+        self.n_mod = len(self.config["modules"])
+        self.yaw_angles = [0] * self.n_mod
+
         # subscribe to remote (parsed by teleop_twist_joy)
         remote_sub = self.create_subscription(
             Twist,
@@ -29,7 +32,7 @@ class Agevar(Node):
 
         self.joint_subs = []
         self.motors_pubs = []
-        for i in range(len(self.config["modules"])):
+        for i in range(self.n_mod):
             info = self.config["modules"][i]
 
             # subscribe to feedback from joints
@@ -54,8 +57,43 @@ class Agevar(Node):
     def remote_callback(self, msg):
         self.get_logger().info(f"Twist x:{msg.linear.x}, y:{msg.linear.x}")
 
-    def robot_feedback_callback(self, msg, module_num):
-        pass
+        # TODO: read data from Twist (parsed by teleop_twist_joy)
+        linear_vel = msg.lin_vel
+        angular_vel = msg.ang_vel
+        sign = msg.sign
+
+        modules = list(range(self.n_mod))
+        if sign == 0:  # going backwards
+            modules.reverse()
+
+        for mod_id in modules:
+            # TODO: compute and publish motor setpoints
+
+            if mod_id != modules[-1]:  # for every module except the last one
+                linear_vel, angular_vel = kinematic(
+                    linear_vel, angular_vel, self.yaw_angles[mod_id])
+
+    # set yaw angle of a joint
+    def yaw_feedback_callback(self, msg, module_num):
+        angle = msg.data
+        if angle >= 180:
+            angle -= 360
+        self.yaw_angles[module_num-17] = angle
+
+    # given data of a module, compute linear and angular velocities of the next one
+    def kinematic(linear_vel, angular_vel, yaw_angle):
+        delta_dot = -(angular_vel * (rc.b + rc.a * cos(yaw_angle)) +
+                      linear_vel * sin(yaw_angle)) / rc.b
+        yaw_angle = yaw_angle + delta_dot * rc.Ts
+
+        angular_out = angular_vel + delta_dot
+
+        linear_out_x = linear_vel + rc.b * sin(yaw_angle) * angular_out
+        linear_out_y = -angular_vel * rc.a - \
+            rc.b * cos(yaw_angle) * angular_out
+        linear_out = sqrt(linear_out_x**2 + linear_out_y**2)
+
+        return linear_out, angular_out
 
 
 def main(args=None):
