96b97f50ddcbf079fdae30a0643b8ffaada416ba
Tizo0o0o0o <gugliandolosergio@gmail.com>
2023-05-29 23:56:10 +0200

    more space in joint area, still miss feedback yaw

--- reseq_ros2/reseq_ros2/constants.py
+++ reseq_ros2/reseq_ros2/constants.py
@@ -32,8 +32,8 @@ topic_to_id = {
 ### Agevar ###
 
 # GEOMETRIC CONSTANTS
-a = 0.15        # [m] Distance between the center of every module and the previous yaw joint
-b = 0.1         # [m] Distance between the center of every module and the following yaw joint
+a = 0.2         # [m] Distance between the center of every module and the previous yaw joint
+b = 0.15        # [m] Distance between the center of every module and the following yaw joint
 d = 0.13        # [m] Distance between pair of equivalent wheels
 r_eq = 0.06     # [m] Radius of the equivalent wheels
 
