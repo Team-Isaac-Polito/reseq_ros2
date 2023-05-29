2aa8b306c3b1faece1ec0cbe09f11da5cd522516
Sergio Gugliandolo <65220960+Tizio0o0o0o@users.noreply.github.com>
2023-05-29 19:26:14 +0200

    Added "gazebo" to try to test agevar in it
    The main Gazebo repo will still be used for a little bit, this is just the beginning :)

---
+++ gazebo/README.md
@@ -0,0 +1,68 @@
+# Gazebo
+
+## This is the base to test the robot on Gazebo.
+
+---
+
+---
+
+## Prerequisites:
+
+- install gazebo and other packages as shown below
+
+```
+curl -sSL http://get.gazebosim.org | sh # not quite sure that this is the correct way, if something does not work try other methods ad report the mistake
+sudo apt-get install ros-humble-gazebo-ros2-control
+sudo apt-get install ros-humble-xacro
+sudo apt-get install ros-humble-diff-drive-controller
+sudo apt-get install ros-humble-joint-state-broadcaster
+#sudo apt-get install ros-humble-position-controllers !!! NOT CURRENTLY USED !!!
+sudo apt-get install ros-humble-teleop-twist-keyboard
+sudo apt-get install ros-humble-rviz2
+```
+
+---
+
+## To launch the simulation
+
+open 2 or 3 terminal (remember one time at the opening of the terminal to do `source install/setup.bash` from `dev_ws` folder)
+
+In terminal 1 (to launch many scripts)
+
+```
+ros2 launch reseq launch_sim.launch.py
+```
+
+In terminal 2 (to launch the controller. You must select this terminal and type in keyboard to control the robot)
+
+```
+ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r /cmd_vel:=/diff_cont_1/cmd_vel_unstamped
+```
+
+remember that you have to select this terminal to give commands to the robot in Gazebo
+
+> :warning: In this way you are only controlling the first module, the 3 module behing are "trying to stop" it. This because currently there are 4 differential drive controller (one for each module) that expect to read the linear and angular speed from
+
+```
+/cmd_vel:=/diff_cont_1/cmd_vel_unstamped # module 1
+/cmd_vel:=/diff_cont_2/cmd_vel_unstamped # module 2
+/cmd_vel:=/diff_cont_3/cmd_vel_unstamped # module 3
+/cmd_vel:=/diff_cont_4/cmd_vel_unstamped # module 4
+```
+
+> :warning: The correct way would be to send the twist command to AGEVAR and AGEVAR should send the twist command for each module.
+
+In terminal 3 (if you want to see the robot in Rviz too)
+
+```
+
+rviz2
+
+```
+
+- set "fixed frame" from "map" to "base link" (to refer to the robot body)
+- add "TF" to visualize all tf from the robot parts (and see if everything is moving as it should)
+- add "RobotModel" and change "Description Topic" to "/robot_description" to visualize the shape of the robot (You can actually decide to visualize "Visual" or "Collision" property of the robot)
+- a final ctr-s wil save your configuration for next time you will use Rviz2
+
+To terminate the process type ctrl-c (there are many running services, not only Gazebo client).
---
+++ gazebo/launch/launch_sim.launch.py
@@ -0,0 +1,84 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+
+    package_name = 'reseq'
+
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(
+                package_name), 'launch', 'rsp.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+    )
+
+    gazebo_params_file = os.path.join(get_package_share_directory(
+        package_name), 'config', 'gazebo_params.yaml')
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+        launch_arguments={
+            'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+    )
+
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'reseq'],
+                        output='screen')
+    
+    
+    diff_drive_1_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont_1"],
+    )
+    
+    diff_drive_2_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont_2"],
+    )
+    
+    diff_drive_3_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont_3"],
+    )
+    
+    diff_drive_4_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont_4"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
+    # Launch them all!
+    return LaunchDescription([
+        rsp,
+        gazebo,
+        spawn_entity,
+        diff_drive_1_spawner,
+        diff_drive_2_spawner,
+        diff_drive_3_spawner,
+        diff_drive_4_spawner,
+        joint_broad_spawner
+    ])
