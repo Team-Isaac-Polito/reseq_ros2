68d7b089b3346729af0ca5da790c3e663b7012c5
GBergatto <gb.bergatto@gmail.com>
2023-06-07 09:49:22 +0200

    Add ROS parameters to communication node

--- reseq_ros2/reseq_ros2/communication.py
+++ reseq_ros2/reseq_ros2/communication.py
@@ -2,35 +2,40 @@ import can
 import rclpy
 import reseq_ros2.constants as rc
 import struct
-import yaml
 from can import Message
 from rclpy.node import Node
 from reseq_interfaces.msg import Motors
 from std_msgs.msg import Float32  # deprecated?
-from yaml.loader import SafeLoader
 
 
 class Communication(Node):
     def __init__(self):
         super().__init__("communication")
-        # TODO: read file passed as ROS argument
-        with open("src/reseq_ros2/config/config_mk2.yaml") as f:
-            self.config = yaml.load(f, Loader=SafeLoader)
-            print(self.config)
+
+        # define ROS parameters
+        self.can_channel = self.declare_parameter(
+            "can_channel", "vcan0").get_parameter_value().string_value
+        self.modules = self.declare_parameter(
+            "modules", [0]).get_parameter_value().integer_array_value
+        self.joints = self.declare_parameter(
+            "joints", [0]).get_parameter_value().integer_array_value
+
+        self.get_logger().info(
+            f"Starting with parameters can_channel: {self.can_channel}, modules: {self.modules.tolist()}, joints: {self.joints.tolist()}")
 
         # create ROS publishers and subscribers for each module
         self.pubs = []
         self.subs = []
-        for i in range(len(self.config["modules"])):
+        for i in range(len(self.modules)):
             self.pubs.append(self.create_module_pubs(
-                self.config["modules"][i]))
+                self.modules[i], self.modules[i] in self.joints))
             self.subs.append(self.create_module_subs(
-                self.config["modules"][i]))
+                self.modules[i], self.modules[i] in self.joints))
 
         # connect to CAN bus
         try:
             self.canbus = can.interface.Bus(
-                channel=self.config["canbus"]["channel"],
+                channel=self.can_channel,
                 bustype="socketcan",
             )
             self.notifier = can.Notifier(self.canbus, [self.can_callback])
@@ -39,32 +44,32 @@ class Communication(Node):
 
         self.get_logger().info("Communication node started")
 
-    def create_module_pubs(self, info):
+    def create_module_pubs(self, address, hasJoint):
         d = {}
         for subtopic in rc.id_to_topic.values():
-            if subtopic.split("/")[0] == "joint" and info["hasJoint"] == False:
+            if subtopic.split("/")[0] == "joint" and not hasJoint:
                 continue
 
             d[subtopic] = self.create_publisher(
                 Motors if subtopic.split("/")[0] == "motor" else Float32,
-                f"reseq/module{info['address']}/{subtopic}",
+                f"reseq/module{address}/{subtopic}",
                 10,
             )
 
         return d
 
-    def create_module_subs(self, info):
+    def create_module_subs(self, address, hasJoint):
         d = {}
         for subtopic in rc.topic_to_id.keys():
-            if subtopic.split("/")[0] == "joint" and info["hasJoint"] == False:
+            if subtopic.split("/")[0] == "joint" and not hasJoint:
                 continue
 
             d[subtopic] = self.create_subscription(
                 Motors if subtopic.split("/")[0] == "motor" else Float32,
-                f"reseq/module{info['address']}/{subtopic}",
+                f"reseq/module{address}/{subtopic}",
                 # use lamba function to pass extra arguments to the callback
                 lambda msg, s=subtopic: self.ros_listener_callback(
-                    msg, info["address"], s),
+                    msg, address, s),
                 10,
             )
 
