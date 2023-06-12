4a9232a473ddb054e590d0b1f5a4474bd40532ee
GBergatto <gb.bergatto@gmail.com>
2023-06-12 08:55:49 +0200

    Re-make Agevar compatible with physical robot
    Publish to both /motor/setpoint and topics used by diff_drive_controller
    
    Fix #7

--- reseq_ros2/reseq_ros2/agevar.py
+++ reseq_ros2/reseq_ros2/agevar.py
@@ -1,7 +1,9 @@
 import rclpy
+import reseq_ros2.constants as rc
 from math import cos, sin, pi
 from geometry_msgs.msg import Twist
 from rclpy.node import Node
+from reseq_interfaces.msg import Motors
 from std_msgs.msg import Float32  # deprecated?
 
 
@@ -13,8 +15,10 @@ class Agevar(Node):
         self.joints = self.declare_parameter("joints", [0]).get_parameter_value().integer_array_value
         self.a = self.declare_parameter("a", 0.0).get_parameter_value().double_value
         self.b = self.declare_parameter("b", 0.0).get_parameter_value().double_value
+        self.d = self.declare_parameter("d", 0.0).get_parameter_value().double_value
+        self.r_eq = self.declare_parameter("r_eq", 0.0).get_parameter_value().double_value
 
-        self.get_logger().info(f'Starting with parameters a: {self.a}, b: {self.b}, modules: {self.modules.tolist()}, joints: {self.joints.tolist()}')
+        self.get_logger().info(f"Starting with parameters a: {self.a}, b: {self.b}, d: {self.d}, r_eq: {self.r_eq}, modules: {self.modules.tolist()}, joints: {self.joints.tolist()}")
 
         self.n_mod = len(self.modules)
         self.yaw_angles = [0] * self.n_mod
@@ -29,6 +33,7 @@ class Agevar(Node):
 
         self.joint_subs = []
         self.motors_pubs = []
+        self.diff_pubs = []
         for i in range(self.n_mod):
             address = self.modules[i]
 
@@ -37,19 +42,26 @@ class Agevar(Node):
                 s = self.create_subscription(
                     Float32,
                     f"reseq/module{address}/joint/yaw/feedback",
-                    lambda msg, x=address: self.yaw_feedback_callback(
-                        msg, x),
+                    lambda msg, x=address: self.yaw_feedback_callback(msg, x),
                     10,
                 )
                 self.joint_subs.append(s)
 
             # create publisher for the motor topics
             p = self.create_publisher(
+                Motors,
+                f"reseq/module{address}/motor/setpoint",
+                10
+            )
+            self.motors_pubs.append(p)
+
+            # create publisher for diff_drive_controller
+            p = self.create_publisher(
                 Twist,
                 f"diff_cont_{address}/cmd_vel_unstamped",
                 10
             )
-            self.motors_pubs.append(p)
+            self.diff_pubs.append(p)
             
         self.get_logger().info("Agevar node started!")
 
@@ -65,13 +77,19 @@ class Agevar(Node):
             angular_vel = -angular_vel
 
         for mod_id in modules:
-            # TODO: compute and publish motor setpoints
+            wdx, wsw = self.vel_motors(linear_vel, angular_vel, sign)
 
-            # publish to ROS diff_drive_controller topics
+            # publish to ROS motor topics
+            m = Motors()
+            m.right = wdx
+            m.left = wsw
+            self.motors_pubs[mod_id].publish(m)
+
+            # publish to diff_drive_controller
             m = Twist()
             m.linear.x = linear_vel
             m.angular.z = angular_vel
-            self.motors_pubs[mod_id].publish(m)
+            self.diff_pubs[mod_id].publish(m)
 
             if mod_id != modules[-1]:  # for every module except the last one
                 linear_vel, angular_vel = self.kinematic(
@@ -94,6 +112,21 @@ class Agevar(Node):
         angular_out = (linear_vel * sin(yaw_angle) - self.a * angular_vel * cos(yaw_angle)) / self.b
         
         return linear_out, angular_out
+
+    # compute motors velocity for each module
+    def vel_motors(self, lin_vel, ang_vel, sign):
+        wdx = (lin_vel + ang_vel * self.d / 2) / self.r_eq
+        wsx = (lin_vel - ang_vel * self.d / 2) / self.r_eq
+
+        if sign == 0:  # backwards
+            wsx, wdx = -wsx, -wdx
+
+        # from radiants to rmp
+        wdx = wdx * rc.rads2rpm
+        wsx = wsx * rc.rads2rpm
+
+        return wdx, wsx
+
 
 def main(args=None):
     rclpy.init(args=args)
