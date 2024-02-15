6497fd189a566a2662db6d48fc0bf8ca93370eeb
Luca De Paola <60526200+luca-byte@users.noreply.github.com>
2024-02-15 01:35:15 +0100

    build: Add launch file detection

--- reseq_ros2/setup.py
+++ reseq_ros2/setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from glob import glob
 
 package_name = 'reseq_ros2'
 
@@ -7,6 +8,7 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
+        (f'share/{package_name}/launch', glob('launch/*launch.py')),
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
