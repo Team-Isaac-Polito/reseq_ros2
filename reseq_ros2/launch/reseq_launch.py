fbc92195558f3d7dc06d4df31734de337551a59e
lorixhack <103076648+lorixhack@users.noreply.github.com>
2024-04-25 22:04:32 +0200

    feat: possibility to get the config filename from command line

--- reseq_ros2/launch/reseq_launch.py
+++ reseq_ros2/launch/reseq_launch.py
@@ -1,117 +1,112 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-import sys
+from launch.actions import OpaqueFunction
 import yaml
 from yaml import SafeLoader
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 share_folder = get_package_share_directory("reseq_ros2")
-
-def get_config():
-    args = sys.argv
-    if len(args)>1:
-        filename = share_folder + args[1]
-    else:
-        filename = share_folder + "/config/reseq_mk1_can.yaml"
-    return filename
+default_path = "/config/reseq_mk1_can.yaml"
 
 def parse_config(filename):
     with open(filename) as f:
         return yaml.load(f, Loader=SafeLoader)
     
-def declare_parameters(config):
-    #Declare each parameter present in the yaml fle as a Launch Argument
-    args = []
-    for k,v in config.items():
-        if k=='geometric_consts':
-            for name, val in v.items():
-                args.append(DeclareLaunchArgument(name, default_value = str(val)))
-        elif k=='enea_consts':
-            for name, val in v.items():
-                args.append(DeclareLaunchArgument(name, default_value = str(val)))
-        elif k=='modules':
-            addresses = []
-            joints = []
-            endEffectors = []
-            for mod in v:
-                addresses.append(mod['address'])
-                if mod['hasJoint']:
-                    joints.append(mod['address'])
-                if mod['hasEndEffector']:
-                    endEffectors.append(mod['address'])
-            args.append(DeclareLaunchArgument('modules', default_value = str(addresses)))
-            args.append(DeclareLaunchArgument('joints', default_value = str(joints)))
-            args.append(DeclareLaunchArgument('end_effectors', default_value = str(endEffectors)))
-        elif k=='canbus':
-            args.append(DeclareLaunchArgument('can_channel', default_value = v['channel']))
-    return args
+def get_addresses(config):
+    l = []
+    for mod in config['modules']:
+        l.append(mod['address'])
+    return l
 
-def generate_launch_description():
-    config = parse_config(get_config())
-    ld =  LaunchDescription(declare_parameters(config) + [
-        Node(
+def get_joints(config):
+    l = []
+    for mod in config['modules']:
+        if mod['hasJoint']:
+            l.append(mod['address'])
+    return l
+
+def get_end_effectors(config):
+    l = []
+    for mod in config['modules']:
+        if mod['hasEndEffector']:
+            l.append(mod['address'])
+    return l
+    
+def launch_setup(context, *args, **kwargs):
+    config_path = LaunchConfiguration('config_path').perform(context)
+    config = parse_config(share_folder + config_path)
+    addresses = get_addresses(config)
+    joints = get_joints(config)
+    endEffectors = get_end_effectors(config)
+    nodes = []
+    nodes.append(Node(
             package='reseq_ros2',
             executable='communication',
             name='communication',
             parameters=[{
-                'can_channel': LaunchConfiguration('can_channel'),
-                'modules': LaunchConfiguration('modules'),
-                'joints': LaunchConfiguration('joints'),
-                'end_effectors': LaunchConfiguration('end_effectors')
-            }]
-        ),
-        Node(
+                'can_channel': config['canbus']['channel'],
+                'modules': addresses,
+                'joints': joints,
+                'end_effectors': endEffectors
+            }]))
+    nodes.append(Node(
             package='reseq_ros2',
             executable='agevar',
             name='agevar',
             parameters=[{
-                'a': LaunchConfiguration('a'),
-                'b': LaunchConfiguration('b'),
-                'd': LaunchConfiguration('d'),
-                'r_eq': LaunchConfiguration('r_eq'),
-                'modules': LaunchConfiguration('modules'),
-                'joints': LaunchConfiguration('joints'),
-                'end_effectors': LaunchConfiguration('end_effectors')
-            }]
-        ),
-        Node(
+                'a': config['dimensions']['a'],
+                'b': config['dimensions']['b'],
+                'd': config['dimensions']['d'],
+                'r_eq': config['dimensions']['r_eq'],
+                'modules': addresses,
+                'joints': joints,
+                'end_effectors': endEffectors
+            }]))
+    nodes.append(Node(
             package='reseq_ros2',
             executable='scaler',
             name='scaler',
-        ),
-        Node(
+            parameters=[{
+                'r_linear_vel': config['dimensions']['r_linear_vel'],
+                'r_inverse_radius': config['dimensions']['r_inverse_radius'],
+                'r_pitch_vel': config['dimensions']['r_pitch_vel'],
+                'r_head_pitch_vel': config['dimensions']['r_head_pitch_vel'],
+                'r_head_yaw_vel': config['dimensions']['r_head_yaw_vel'],
+            }]))
+    nodes.append(Node(
             package='realsense2_camera',
             executable='realsense2_camera_node',
             name='realsense2_camera_node',
             parameters=[{
                 'config_file': share_folder + "/config/realsense_rgb_motion.yaml"
-            }]
-        )
-        #Node(
-        #    package='reseq_ros2',
-        #    executable='remote_test',
-        #    name='remote_test',
-        #),
-    ])
-    #Launch EnEA conditionally to the version
+            }]))
     if config['version'] == 'mk1':
-        ld.add_action(
-            Node(
+        nodes.append(Node(
                 package='reseq_ros2',
                 executable='enea',
                 name='enea',
                 parameters=[{
-                    'pitch': LaunchConfiguration('i_pitch'),
-                    'head_pitch': LaunchConfiguration('i_head_pitch'),
-                    'head_yaw': LaunchConfiguration('i_head_yaw'),
-                    'servo_speed': LaunchConfiguration('servo_speed'),
-                    'r_pitch': LaunchConfiguration('r_pitch'),
-                    'r_head_pitch': LaunchConfiguration('r_head_pitch'),
-                    'r_head_yaw': LaunchConfiguration('r_head_yaw'),
-                    'pitch_conv': LaunchConfiguration('pitch_conv'),
-                    'end_effectors': LaunchConfiguration('end_effectors')
-                }]
-        ))
-    return ld
+                    'pitch': config['enea_consts']['i_pitch'],
+                    'head_pitch': config['enea_consts']['i_head_pitch'],
+                    'head_yaw': config['enea_consts']['i_head_yaw'],
+                    'servo_speed': config['enea_consts']['servo_speed'],
+                    'r_pitch': config['enea_consts']['r_pitch'],
+                    'r_head_pitch': config['enea_consts']['r_head_pitch'],
+                    'r_head_yaw': config['enea_consts']['r_head_yaw'],
+                    'pitch_conv': config['enea_consts']['pitch_conv'],
+                    'end_effectors': endEffectors
+                }]))
+    return nodes
+    
+def generate_launch_description():
+    return LaunchDescription([DeclareLaunchArgument('config_path', default_value=default_path), 
+                             OpaqueFunction(function = launch_setup)
+                             ])
+
+ #Node(
+        #    package='reseq_ros2',
+        #    executable='remote_test',
+        #    name='remote_test',
+        #),
