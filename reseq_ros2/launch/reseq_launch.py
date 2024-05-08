4f0e05156beeba450b37858ad6077dd639e804f5
lorixhack <103076648+lorixhack@users.noreply.github.com>
2024-05-08 11:20:49 +0200

    feat: launch robot_state_publisher node

--- reseq_ros2/launch/reseq_launch.py
+++ reseq_ros2/launch/reseq_launch.py
@@ -6,6 +6,7 @@ import yaml
 from yaml import SafeLoader
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+import xacro
 
 #Default config file path
 share_folder = get_package_share_directory("reseq_ros2")
@@ -100,6 +101,16 @@ def launch_setup(context, *args, **kwargs):
                     'pitch_conv': config['enea_consts']['pitch_conv'],
                     'end_effector': endEffector
                 }]))
+    
+    xacro_file = share_folder + "/description/robot.urdf.xacro"
+    robot_description = xacro.process_file(xacro_file, mappings={'config_path': share_folder + config_path}).toxml()
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description}] # add other parameters here if required
+    )
+    nodes.append(robot_state_publisher_node)
     return nodes
     
 def generate_launch_description():
