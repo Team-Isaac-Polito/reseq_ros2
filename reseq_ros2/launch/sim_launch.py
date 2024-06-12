1866f104882799428a4790524714c8027d91459f
lorixhack <103076648+lorixhack@users.noreply.github.com>
2024-06-12 09:35:48 +0200

    fix: correction of gazebo launch file

--- reseq_ros2/launch/sim_launch.py
+++ reseq_ros2/launch/sim_launch.py
@@ -2,6 +2,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
+from launch.actions import ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
  
  
@@ -26,12 +27,16 @@ def generate_launch_description():
             )
 
     #Include the executable can spawn the robot model in the simulation
-    spawn_entity = Node(package='ros_gz_sim', executable='create',
-                        arguments=['-topic', 'robot_description', 
-                                   '-entity', 'reseq_ros2'],
-                        output='screen')               
+    spawn_entity = Node(
+            package='ros_gz_sim', 
+            executable='create',
+            arguments=['-name', 'reseq', '-topic', '/robot_description'],
+            output='screen')
 
     return LaunchDescription([
+        ExecuteProcess(
+            cmd=['gz', 'sim', '-v', '4', '-s', 'libgazebo_ros_init.so', 'empty.sdf'],
+            output='screen'),
         rsp,
         gazebo,
         spawn_entity
