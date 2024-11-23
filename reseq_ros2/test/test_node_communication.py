b70c2d6fa56375178d50e861fc4a2c644b7a585d
Yunus-Selim <yunusselimaytekin@gmail.com>
2024-11-23 11:47:00 +0100

    refactor(tests): Improve problem reporting in assertions and remove TODOs

--- reseq_ros2/test/test_node_communication.py
+++ reseq_ros2/test/test_node_communication.py
@@ -13,7 +13,7 @@ import launch_testing
 import launch_testing.actions
 
 class TestNodes(unittest.TestCase):
-    # 1. TODO: get values from .yaml files.
+
     @classmethod
     def setUpClass(cls):
         rclpy.init()
@@ -27,20 +27,23 @@ class TestNodes(unittest.TestCase):
         cls.msg.left = Vector3(x=0.5, y=-0.2, z=1.0)
         cls.msg.right = Vector3(x=-0.8, y=0.3, z=0.7)
 
-        cls.address = 17 # TODO: 1.
+        cls.address = 17
 
-        cls.battery_num = 3 # TODO: 1.
-        cls.motor_num = 3 # TODO: 1.
-        cls.joint_num = 6 # TODO: 1.
-        cls.end_effector_num = 3 # TODO: 1.
+        cls.module_num = 3
+        cls.joint_num = 6
+        cls.end_effector_num = 3
 
-        cls.module_num = 3 # TODO: 1.
+        cls.motor_type = Motors
+        cls.joint_type = Float32
+        cls.end_effector_type = Int32
+        cls.diff_controller_type = TwistStamped
 
     @classmethod
     def tearDownClass(cls):
         rclpy.shutdown()
 
     def setUp(self):
+        self.topic_names, self.topic_types = map(list, zip(*self.node.get_topic_names_and_types()))
         self.msgs = [None] * 16
 
     def tearDown(self):
@@ -69,22 +72,27 @@ class TestNodes(unittest.TestCase):
             if self.msgs[0] and self.msgs[1]:
                 break
 
-        self.assertTrue(self.msgs[0] or self.msgs[1], "No topic found.")
         problems = []
         if not bool(self.msgs[0]): problems.append("Couldn't get message from '/end_effector' topic.")
         if not bool(self.msgs[1]): problems.append("Couldn't get message from '/cmd_vel' topic.")
         self.assertTrue(len(problems) == 0, f"\n{''.join(problems)}")
+        time.sleep(2) # Wait until launch ends
 
     def test_2_agevar_topics(self):
-        time.sleep(2) # Wait until launch ends
-        topic_list = [t[0] for t in self.node.get_topic_names_and_types()] # TODO: 1.
         idx = 0
         while 1:
             topic = f'/reseq/module{self.address + idx}/motor/setpoint'
-            if topic not in topic_list:
+            if topic not in self.topic_names:
                 break
+
+            # Test topic type
+            arg = self.topic_names.index(topic)
+            expected_type_str = f"{self.motor_type.__module__.split('._')[0]}._{self.motor_type.__name__}"
+            topic_type_str = self.topic_types[arg][0].replace('msg/', 'msg._').replace('/', '.')
+            self.assertEqual(topic_type_str, expected_type_str, f"The type of the topic {topic} is {topic_type_str}. Expected {expected_type_str}")
+
             self.node.create_subscription(
-                Motors,
+                self.motor_type,
                 topic,
                 self.create_callback(idx),
                 10
@@ -99,23 +107,29 @@ class TestNodes(unittest.TestCase):
             if None not in self.msgs[:idx]:
                 break
 
-        self.assertTrue(self.msgs[:idx], "No topic found.")
         problems = []
+        if len(self.msgs[:idx]) != self.module_num: problems.append(f"Number of topics doesn't match. Expected {self.module_num}, got {len(self.msgs[:idx])}.")
         for i in range(idx):
             if not bool(self.msgs[i]): problems.append(f"Couldn't get message from '/reseq/module{self.address + i}/motor/setpoint' topic.")
         self.assertTrue(len(problems) == 0, f"\n{''.join(problems)}")
 
     def test_3_enea_topics(self):
-        topic_list = [t[0] for t in self.node.get_topic_names_and_types()] # TODO: 1.
         idx = 0
         for vel in ["pitch", "head_pitch", "head_roll"]:
             n = 0
             while 1:
                 topic = f'/reseq/module{self.address + n}/end_effector/{vel}/setpoint'
-                if topic not in topic_list:
+                if topic not in self.topic_names:
                     break
+
+                # Test topic type
+                arg = self.topic_names.index(topic)
+                expected_type_str = f"{self.end_effector_type.__module__.split('._')[0]}._{self.end_effector_type.__name__}"
+                topic_type_str = self.topic_types[arg][0].replace('msg/', 'msg._').replace('/', '.')
+                self.assertEqual(topic_type_str, expected_type_str, f"The type of the topic {topic} is {topic_type_str}. Expected {expected_type_str}")
+                
                 self.node.create_subscription(
-                    Int32,
+                    self.end_effector_type,
                     topic,
                     self.create_callback(idx),
                     10
@@ -131,38 +145,38 @@ class TestNodes(unittest.TestCase):
             if None not in self.msgs[:idx]:
                 break
 
-        self.assertTrue(self.msgs[:idx], "No topic found.")
         problems = []
+        if len(self.msgs[:idx]) != self.end_effector_num: problems.append(f"Number of topics doesn't match. Expected {self.end_effector_num}, got {len(self.msgs[:idx])}.")
         for i in range(idx):
             if not bool(self.msgs[i]): problems.append(f"Couldn't get message from '/reseq/module{self.address + (i % (idx//3))}/end_effector/{['pitch', 'head_pitch', 'head_roll'][i // (idx//3)]}/setpoint' topic.")
         self.assertTrue(len(problems) == 0, f"\n{''.join(problems)}")
 
     def test_4_communication_topics(self):
-        topic_names, topic_types = map(list, zip(*self.node.get_topic_names_and_types())) # TODO: 1.
         idx = 0
         n_motor = -1
         n_joint = -1
         n_end_effector = -1
-        for st in ['motor', 'joint', 'end_effector']: # TODO: Add battery
+        for st in ['motor', 'joint', 'end_effector']:
             while 1:
                 if st == 'motor':
                     topic = f'/reseq/module{self.address + idx}/{st}/feedback'
-                    topic_type = Motors
+                    topic_type = self.motor_type
                     n_motor += 1
                 elif st == 'joint':
                     topic = f"/reseq/module{self.address+1 + ((idx-n_motor)//3)}/{st}/{['pitch', 'roll', 'yaw'][(idx-n_motor)%3]}/feedback"
-                    topic_type = Float32
+                    topic_type = self.joint_type
                     n_joint += 1
                 else:
                     topic = f"/reseq/module{self.address + ((idx-n_motor-n_joint)//3)}/{st}/{['pitch', 'head_pitch', 'head_roll'][(idx-n_motor-n_joint)%3]}/feedback"
-                    topic_type = Int32
+                    topic_type = self.end_effector_type
                     n_end_effector += 1
-                if topic not in topic_names:
+                if topic not in self.topic_names:
                     break
+
                 # Test topic type
-                arg = topic_names.index(topic)
+                arg = self.topic_names.index(topic)
                 expected_type_str = f"{topic_type.__module__.split('._')[0]}._{topic_type.__name__}"
-                topic_type_str = topic_types[arg][0].replace('msg/', 'msg._').replace('/', '.')
+                topic_type_str = self.topic_types[arg][0].replace('msg/', 'msg._').replace('/', '.')
                 self.assertEqual(topic_type_str, expected_type_str, f"The type of the topic {topic} is {topic_type_str}. Expected {expected_type_str}")
 
                 self.node.create_subscription(
@@ -180,10 +194,9 @@ class TestNodes(unittest.TestCase):
             rclpy.spin_once(self.node, timeout_sec=0.1)
             if None not in self.msgs[:idx]:
                 break
-        
-        self.assertTrue(self.msgs[:idx], "No topic found.")
+
         problems = []
-        if n_motor != self.motor_num: problems.append(f"Number of motors doesn't match. Expected {self.motor_num}, got {n_motor}.")
+        if n_motor != self.module_num: problems.append(f"Number of motors doesn't match. Expected {self.module_num}, got {n_motor}.")
         if n_joint != self.joint_num: problems.append(f"Number of joints doesn't match. Expected {self.joint_num}, got {n_joint}.")
         if n_end_effector != self.end_effector_num: problems.append(f"Number of end effectors doesn't match. Expected {self.end_effector_num}, got {n_end_effector}.")
         for i in range(idx):
@@ -204,9 +217,17 @@ class TestNodes(unittest.TestCase):
         )
         idx = self.module_num + 1
         for i in range(1, idx):
+            topic = f"/diff_controller{i}/cmd_vel"
+
+            # Test topic type
+            arg = self.topic_names.index(topic)
+            expected_type_str = f"{self.diff_controller_type.__module__.split('._')[0]}._{self.diff_controller_type.__name__}"
+            topic_type_str = self.topic_types[arg][0].replace('msg/', 'msg._').replace('/', '.')
+            self.assertEqual(topic_type_str, expected_type_str, f"The type of the topic {topic} is {topic_type_str}. Expected {expected_type_str}")
+
             self.node.create_subscription(
-                TwistStamped,
-                f"/diff_controller{i}/cmd_vel",
+                self.diff_controller_type,
+                topic,
                 self.create_callback(i),
                 10
             )
@@ -219,7 +240,6 @@ class TestNodes(unittest.TestCase):
             if None not in self.msgs[:idx]:
                 break
 
-        self.assertTrue(self.msgs[:idx], "No topic found.")
         problems = []
         if not bool(self.msgs[0]): problems.append("Couldn't get message from '/joint_states' topic.")
         for i in range(1, idx):
@@ -232,7 +252,6 @@ class TestNodes(unittest.TestCase):
         return callback
 
 def generate_test_description():
-    # TODO: modify the config_file parameter.
     return LaunchDescription([
         ExecuteProcess(
             cmd=["ros2", "launch", "reseq_ros2", "reseq_launch.py", "config_file:=reseq_mk1_vcan.yaml"],
