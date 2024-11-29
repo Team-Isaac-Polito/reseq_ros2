8a39f7ba31b82e7b19d62cdcaca63e6c99981090
Yunus-Selim <yunusselimaytekin@gmail.com>
2024-11-29 12:54:03 +0100

    test(topic): Add topic correctness tests
    - Implement tests to ensure the accuracy of messages published on various topics
    - Validate message contents against expected values
    - Check the correctness of the message frequency

---
+++ reseq_ros2/test/test_topic_correctness.py
@@ -0,0 +1,304 @@
+import importlib
+import struct
+import time
+import unittest
+
+import can
+import geometry_msgs.msg
+import launch_testing
+import launch_testing.actions
+import rclpy
+import sensor_msgs.msg
+import std_msgs.msg
+from geometry_msgs.msg import Vector3
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from rclpy.node import Node
+
+import reseq_interfaces.msg
+from reseq_interfaces.msg import Remote
+
+
+class FrequencyChecker(Node):
+    def __init__(self, topics, string_to_msg_type, pub, msg, canbus):
+        super().__init__('frequency_checker')
+
+        self.subscriptions_ = {}
+        self.intervals = {}
+        self.pub_ = pub
+        self.msg = msg
+        self.canbus = canbus
+
+        for topic_name, msg_type in topics:
+            self.subscriptions_[topic_name] = self.create_subscription(
+                string_to_msg_type(msg_type[0]),
+                topic_name,
+                self.create_callback(topic_name),
+                10
+            )
+            self.intervals[topic_name] = {
+                'last_time': None,
+                'intervals': []
+            }
+        self.timer1 = self.create_timer(0.08, self.publisher_callback) # 12.5 Hz
+        self.timer2 = self.create_timer(0.01, self.feedback_callback) # 100 Hz
+
+    def create_callback(self, topic_name):
+        def callback(msg):
+            current_time = time.time()
+            last_time = self.intervals[topic_name]['last_time']
+            if last_time is not None:
+                interval = current_time - last_time
+                self.intervals[topic_name]['intervals'].append(interval)
+                if len(self.intervals[topic_name]['intervals']) > 100:
+                    self.intervals[topic_name]['intervals'].pop(0)
+            self.intervals[topic_name]['last_time'] = current_time
+        return callback
+
+    def publisher_callback(self):
+        self.pub_.publish(self.msg)
+    
+    def feedback_callback(self):
+        for id, module_num, data in [(0x22, 0x11, struct.pack('ff', 5.0, 3.0)),
+                                         (0x22, 0x12, struct.pack('ff', 5.0, 3.0)),
+                                         (0x22, 0x13, struct.pack('ff', 5.0, 3.0)),
+                                         (0x32, 0x12, struct.pack('f', 5.0)),
+                                         (0x34, 0x12, struct.pack('f', 5.0)),
+                                         (0x36, 0x12, struct.pack('f', 5.0)),
+                                         (0x32, 0x13, struct.pack('f', 5.0)),
+                                         (0x34, 0x13, struct.pack('f', 5.0)),
+                                         (0x36, 0x13, struct.pack('f', 5.0)),
+                                         (0x42, 0x11, struct.pack('i', 5)),
+                                         (0x44, 0x11, struct.pack('i', 5)),
+                                         (0x46, 0x11, struct.pack('i', 5)),]:
+                # Create a CAN message
+                aid = struct.pack("bbbb", 00, id, 0x00, module_num)
+                msg = can.Message(
+                    arbitration_id=int.from_bytes(aid, byteorder="big", signed=False),
+                    data=data,
+                    is_extended_id=True)
+                # Send the message to trigger the can_callback
+                self.canbus.send(msg)
+
+    def get_frequency(self):
+        freqs = {}
+        for topic_name, data in self.intervals.items():
+            intervals = data['intervals']
+            if intervals:
+                avg_interval = sum(intervals) / len(intervals)
+                frequency = 1.0 / avg_interval if avg_interval > 0 else 0
+                freqs[topic_name] = frequency
+        return freqs
+
+
+class TestNodes(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        rclpy.init()
+        cls.node = rclpy.create_node('test_node')
+        cls.pub_ = cls.node.create_publisher(
+            Remote,
+            "/remote",
+            10
+        )
+        cls.msg = Remote()
+        cls.msg.left = Vector3(x=0.5, y=-0.2, z=1.0)
+        cls.msg.right = Vector3(x=-0.8, y=0.3, z=0.7)
+
+        # Initialize the CAN bus interface
+        cls.canbus = can.interface.Bus(channel='vcan0', bustype='socketcan')
+
+        # Wait for launch
+        time.sleep(5)
+
+    @classmethod
+    def tearDownClass(cls):
+        rclpy.shutdown()
+
+    def setUp(self):
+        self.msgs = [None] * 32
+
+    def tearDown(self):
+        for sub in self.node.subscriptions:
+            self.node.destroy_subscription(sub)
+
+    def test_1_forward(self):
+        """Test the message accuracy of the publisher topics."""
+        # Create the subscribers for each topic
+        topics = [(topic, type) for topic, type in self.node.get_topic_names_and_types()
+                  if len(self.node.get_subscriptions_info_by_topic(topic)) != 0
+                  and 'joint' not in topic.split('/') and topic.split("/")[-1] != 'feedback'
+                  and topic not in ['/joint_states', '/diff_controller1/cmd_vel', '/diff_controller2/cmd_vel', '/diff_controller3/cmd_vel', '/robot_description', '/parameter_events']]
+        topic_names = []
+        idx = 0
+        for topic, type in topics:
+            self.node.create_subscription(
+                self.string_to_msg_type(type[0]),
+                topic,
+                self.create_callback(idx, type),
+                10)
+            topic_names.append(topic)
+            idx += 1
+
+        # Wait until it transmits message
+        endtime = time.time() + 10
+        while time.time() < endtime:
+            self.pub_.publish(self.msg)
+            rclpy.spin_once(self.node, timeout_sec=0)
+            if None not in self.msgs[:idx]:
+                break
+
+        # Test accuracy
+        expected = {
+            '/cmd_vel': geometry_msgs.msg.Twist(linear=geometry_msgs.msg.Vector3(x=0.08105100000000004, y=0.0, z=0.0), angular=geometry_msgs.msg.Vector3(x=0.0, y=0.0, z=0.09975757080000004)),
+            '/end_effector': reseq_interfaces.msg.EndEffector(pitch_vel=-36.599998474121094, head_pitch_vel=400.0, head_roll_vel=227.5),
+            '/remote': reseq_interfaces.msg.Remote(left=geometry_msgs.msg.Vector3(x=0.5, y=-0.2, z=1.0), right=geometry_msgs.msg.Vector3(x=-0.8, y=0.3, z=0.7), buttons=[], switches=[]),
+            '/reseq/module17/end_effector/head_pitch/setpoint': std_msgs.msg.Int32(data=800),
+            '/reseq/module17/end_effector/head_roll/setpoint': std_msgs.msg.Int32(data=0),
+            '/reseq/module17/end_effector/pitch/setpoint': std_msgs.msg.Int32(data=750),
+            '/reseq/module17/motor/setpoint': reseq_interfaces.msg.Motors(left=14.360505104064941, right=18.928958892822266),
+            '/reseq/module18/motor/setpoint': reseq_interfaces.msg.Motors(left=18.749523162841797, right=14.53994083404541),
+            '/reseq/module19/motor/setpoint': reseq_interfaces.msg.Motors(left=14.705280303955078, right=18.584184646606445),
+        }
+        for msg, topic in zip(self.msgs[:idx], topic_names):
+            exp = expected[topic]
+            self.assertEqual(msg, exp, f"The message of {topic} is incorrect. Expected {exp}, got {msg}")
+
+    def test_2_feedback(self):
+        """Test the message accuracy of the telemetry/feedback."""
+        # Create the subscribers for each topic
+        topics = [(topic, type) for topic, type in self.node.get_topic_names_and_types()
+                   if topic.split('/')[-1] == 'feedback'
+                   or topic in ['/joint_states', '/diff_controller1/cmd_vel', '/diff_controller2/cmd_vel', '/diff_controller3/cmd_vel']]
+        topic_names = []
+        idx = 0
+        for topic, type in topics:
+            self.node.create_subscription(
+                self.string_to_msg_type(type[0]),
+                topic,
+                self.create_callback(idx, type),
+                10)
+            topic_names.append(topic)
+            idx += 1
+
+        # Wait until it transmits message
+        endtime = time.time() + 10
+        while time.time() < endtime:
+            for id, module_num, data in [(0x22, 0x11, struct.pack('ff', 5.0, 3.0)),
+                                         (0x22, 0x12, struct.pack('ff', 5.0, 3.0)),
+                                         (0x22, 0x13, struct.pack('ff', 5.0, 3.0)),
+                                         (0x32, 0x12, struct.pack('f', 5.0)),
+                                         (0x34, 0x12, struct.pack('f', 5.0)),
+                                         (0x36, 0x12, struct.pack('f', 5.0)),
+                                         (0x32, 0x13, struct.pack('f', 5.0)),
+                                         (0x34, 0x13, struct.pack('f', 5.0)),
+                                         (0x36, 0x13, struct.pack('f', 5.0)),
+                                         (0x42, 0x11, struct.pack('i', 5)),
+                                         (0x44, 0x11, struct.pack('i', 5)),
+                                         (0x46, 0x11, struct.pack('i', 5)),]:
+                # Create a CAN message
+                aid = struct.pack("bbbb", 00, id, 0x00, module_num)
+                msg = can.Message(
+                    arbitration_id=int.from_bytes(aid, byteorder="big", signed=False),
+                    data=data,
+                    is_extended_id=True)
+                # Send the message to trigger the can_callback
+                self.canbus.send(msg)
+            rclpy.spin_once(self.node, timeout_sec=0)
+            if None not in self.msgs[:idx]:
+                break
+
+        # Test accuracy
+        expected = {
+            '/diff_controller1/cmd_vel': geometry_msgs.msg.TwistStamped(twist=geometry_msgs.msg.Twist(linear=geometry_msgs.msg.Vector3(x=2.10088, y=0.0, z=0.0), angular=geometry_msgs.msg.Vector3(x=0.0, y=0.0, z=-5.7104648002174505))),
+            '/diff_controller2/cmd_vel': geometry_msgs.msg.TwistStamped(twist=geometry_msgs.msg.Twist(linear=geometry_msgs.msg.Vector3(x=2.10088, y=0.0, z=0.0), angular=geometry_msgs.msg.Vector3(x=0.0, y=0.0, z=-5.7104648002174505))),
+            '/diff_controller3/cmd_vel': geometry_msgs.msg.TwistStamped(twist=geometry_msgs.msg.Twist(linear=geometry_msgs.msg.Vector3(x=2.10088, y=0.0, z=0.0), angular=geometry_msgs.msg.Vector3(x=0.0, y=0.0, z=-5.7104648002174505))),
+            '/joint_states': sensor_msgs.msg.JointState(name=['arm_pitch_1_joint', 'arm_head_pitch_1_joint', 'arm_head_roll_1_joint', 'joint_y_2_joint', 'joint_p_2_joint', 'joint_r_2_joint', 'joint_y_3_joint', 'joint_p_3_joint', 'joint_r_3_joint'], position=[0.508446035955, -4.0690485, -2.5949781, 0.08726646259971647, 0.08726646259971647, 0.08726646259971647, 0.08726646259971647, 0.08726646259971647, 0.08726646259971647], velocity=[], effort=[]),
+            '/reseq/module17/end_effector/head_pitch/feedback': std_msgs.msg.Int32(data=5),
+            '/reseq/module17/end_effector/head_roll/feedback': std_msgs.msg.Int32(data=5),
+            '/reseq/module17/end_effector/pitch/feedback': std_msgs.msg.Int32(data=5),
+            '/reseq/module17/motor/feedback': reseq_interfaces.msg.Motors(left=5.0, right=3.0),
+            '/reseq/module18/joint/pitch/feedback': std_msgs.msg.Float32(data=5.0),
+            '/reseq/module18/joint/roll/feedback': std_msgs.msg.Float32(data=5.0),
+            '/reseq/module18/joint/yaw/feedback': std_msgs.msg.Float32(data=5.0),
+            '/reseq/module18/motor/feedback': reseq_interfaces.msg.Motors(left=5.0, right=3.0),
+            '/reseq/module19/joint/pitch/feedback': std_msgs.msg.Float32(data=5.0),
+            '/reseq/module19/joint/roll/feedback': std_msgs.msg.Float32(data=5.0),
+            '/reseq/module19/joint/yaw/feedback': std_msgs.msg.Float32(data=5.0),
+            '/reseq/module19/motor/feedback': reseq_interfaces.msg.Motors(left=5.0, right=3.0),
+        }
+        for msg, topic in zip(self.msgs[:idx], topic_names):
+            exp = expected[topic]
+            if topic in ['/joint_states']:
+                self.assertEqual(msg.position, exp.position, f"The message of {topic} is incorrect. Expected {exp.position}, got {msg.position}")
+                continue
+            if topic in ['/diff_controller1/cmd_vel', '/diff_controller2/cmd_vel', '/diff_controller3/cmd_vel']:
+                self.assertEqual(msg.twist, exp.twist, f"The message of {topic} is incorrect. Expected {exp.twist}, got {msg.twist}")
+                continue
+            self.assertEqual(msg, exp, f"The message of {topic} is incorrect. Expected {exp}, got {msg}")
+
+    def test_3_frequency(self):
+        """Test the frequency accuracy."""
+        topics = [(topic, type) for topic, type in self.node.get_topic_names_and_types()
+                  if topic in ['/cmd_vel', '/end_effector', '/remote', '/reseq/module17/end_effector/head_pitch/feedback', '/reseq/module17/end_effector/head_pitch/setpoint', '/reseq/module17/end_effector/head_roll/feedback', '/reseq/module17/end_effector/head_roll/setpoint', '/reseq/module17/end_effector/pitch/feedback', '/reseq/module17/end_effector/pitch/setpoint', '/reseq/module17/motor/feedback', '/reseq/module17/motor/setpoint', '/reseq/module18/joint/pitch/feedback', '/reseq/module18/joint/roll/feedback', '/reseq/module18/joint/yaw/feedback', '/reseq/module18/motor/feedback', '/reseq/module18/motor/setpoint', '/reseq/module19/joint/pitch/feedback', '/reseq/module19/joint/roll/feedback', '/reseq/module19/joint/yaw/feedback', '/reseq/module19/motor/feedback', '/reseq/module19/motor/setpoint']]
+        node = FrequencyChecker(topics, self.string_to_msg_type, self.pub_, self.msg, self.canbus)
+
+        # Wait to gather frequency info
+        endtime = time.time() + 1
+        while time.time() < endtime:
+            rclpy.spin_once(node, timeout_sec=0)
+
+        # Test accuracy
+        freqs = node.get_frequency()
+        for topic, frequency in freqs.items():
+            if topic.split('/')[-1] == 'feedback':
+                self.assertAlmostEqual(frequency, 100, delta=1, msg=f"The frequency of {topic} is incorrect. Expected {100}±1, got {frequency}")
+                continue
+            else:
+                self.assertAlmostEqual(frequency, 12.5, delta=0.5, msg=f"The frequency of {topic} is incorrect. Expected {12.5}±0.5, got {frequency}")
+
+        node.destroy_node()
+
+    def create_callback(self, index, type=None):
+        twist = geometry_msgs.msg.Twist(linear=geometry_msgs.msg.Vector3(x=0.0, y=0.0, z=0.0), angular=geometry_msgs.msg.Vector3(x=0.0, y=0.0, z=0.0))
+        name = ['left_back_wheel_1_joint', 'right_back_wheel_1_joint', 'right_front_wheel_2_joint', 'left_back_wheel_2_joint', 'right_back_wheel_2_joint', 'right_front_wheel_1_joint', 'left_front_wheel_2_joint', 'left_front_wheel_3_joint', 'right_front_wheel_3_joint', 'left_front_wheel_1_joint', 'left_back_wheel_3_joint', 'right_back_wheel_3_joint']
+        position = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+        def callback(msg):
+            if type == ['geometry_msgs/msg/TwistStamped']:
+                if msg.twist != twist:
+                    self.msgs[index] = msg
+            elif type == ['sensor_msgs/msg/JointState']:
+                if (msg.name != name and all([m!=n for m,n in zip(msg.position.tolist(), position)])):
+                    self.msgs[index] = msg
+            else:
+                #print(f"\n\n{type}\n\n")
+                self.msgs[index] = msg
+        return callback
+
+    def string_to_msg_type(self, type_str):
+        # Split the string to get the package, module, and class
+        package, module, class_name = type_str.split('/')
+        full_module_name = f'{package}.{module}'
+
+        # Dynamically import the module
+        module = importlib.import_module(full_module_name)
+
+        # Get the class from the module
+        msg_type = getattr(module, class_name)
+
+        return msg_type
+
+def generate_test_description():
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=['ros2', 'launch', 'reseq_ros2', 'reseq_launch.py', 'config_file:=reseq_mk1_vcan.yaml'],
+        ),
+        launch_testing.actions.ReadyToTest()
+    ]), {
+        'TestNodes': TestNodes,
+    }
+
+if __name__ == '__main__':
+    unittest.main()
