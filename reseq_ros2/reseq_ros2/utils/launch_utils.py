83801eeffb6ca95f844c99201e5ad89f4c6e792a
Yunus-Selim <yunusselimaytekin@gmail.com>
2025-01-03 16:53:08 +0100

    fix(config): Update default CAN configuration filename to mk2

--- reseq_ros2/reseq_ros2/utils/launch_utils.py
+++ reseq_ros2/reseq_ros2/utils/launch_utils.py
@@ -5,7 +5,7 @@ from yaml import SafeLoader
 # Default config file path
 share_folder = get_package_share_directory('reseq_ros2')
 config_path = f'{share_folder}/config/temp'
-default_filename = 'reseq_mk1_can.yaml'
+default_filename = 'reseq_mk2_can.yaml'
 
 
 def parse_config(filename):
