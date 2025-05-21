ec5067d26798ffb9e18d65a214bea8a793a50c8a
Yunus-Selim <yunusselimaytekin@gmail.com>
2025-05-21 19:54:47 +0200

    style(detector): format code for style errors

--- reseq_ros2/reseq_ros2/detector.py
+++ reseq_ros2/reseq_ros2/detector.py
@@ -9,8 +9,9 @@ import traceback
 
 import sys
 import os
+
 # Add `/src` to PYTHONPATH to import the necessary modules from reseq_cv
-sys.path.append(os.path.expanduser("~/ros2_ws/src"))
+sys.path.append(os.path.expanduser('~/ros2_ws/src'))
 
 from reseq_cv.orientation_detection.concentric_c import OrientationDetection
 from reseq_cv.motion_detection.motion_detection import MotionDetection
@@ -28,24 +29,28 @@ motion detection, qr code detection, and apriltag detection.
 
 
 class Detector(Node):
-    
     def __init__(self):
         super().__init__('detector')
         self.bridge = CvBridge()
 
         # Subscribe to the camera color image topic
         self.color_subscription = self.create_subscription(
-            Image, '/camera/camera/color/image_raw', self.image_callback, 10)
-        
+            Image, '/camera/camera/color/image_raw', self.image_callback, 10
+        )
+
         # Create a publisher for the model output
         self.model_pub = self.create_publisher(Image, '/detector/model_output', 10)
 
         # Set the image size for the YOLO model
         self.img_size = 640
-        
-        # Define the model path        
-        model_path1 = os.path.expanduser("~/ros2_ws/src/reseq_cv/hazmat_detection/best.pt")
-        model_path2 = os.path.expanduser("~/ros2_ws/src/reseq_cv/object_detection/best.pt")
+
+        # Define the model path
+        model_path1 = os.path.expanduser(
+            '~/ros2_ws/src/reseq_cv/hazmat_detection/best.pt'
+        )
+        model_path2 = os.path.expanduser(
+            '~/ros2_ws/src/reseq_cv/object_detection/best.pt'
+        )
 
         # Initialize the YOLO model
         self.model1 = YOLO(model_path1)
@@ -65,12 +70,12 @@ class Detector(Node):
 
         od_frame, _, _ = self.od.process_image(color_image)
         qr_frame = process_qr_codes(od_frame)
-        #at_frame = process_apriltags(qr_frame)
+        # at_frame = process_apriltags(qr_frame)
         color_image = self.md.process_image(qr_frame)
 
         # Predict hazmat
         results = self.model1(img)
-        
+
         # Process results
         for result in results:
             bbox = result.boxes.xyxy
@@ -80,12 +85,26 @@ class Detector(Node):
                 if conf > 0.5:  # Confidence threshold
                     x1, y1, x2, y2 = bbox[i]
                     label = labels[i]
-                    cv2.rectangle(color_image, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
-                    cv2.putText(color_image, f'{label}: {conf:.2f}', (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+                    cv2.rectangle(
+                        color_image,
+                        (int(x1), int(y1)),
+                        (int(x2), int(y2)),
+                        (255, 0, 0),
+                        2,
+                    )
+                    cv2.putText(
+                        color_image,
+                        f'{label}: {conf:.2f}',
+                        (int(x1), int(y1) - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.9,
+                        (255, 0, 0),
+                        2,
+                    )
 
         # Predict object
         results = self.model2(img)
-        
+
         # Process results
         for result in results:
             bbox = result.boxes.xyxy
@@ -95,20 +114,35 @@ class Detector(Node):
                 if conf > 0.3:  # Confidence threshold
                     x1, y1, x2, y2 = bbox[i]
                     label = labels[i]
-                    cv2.rectangle(color_image, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
-                    cv2.putText(color_image, f'{label}: {conf:.2f}', (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+                    cv2.rectangle(
+                        color_image,
+                        (int(x1), int(y1)),
+                        (int(x2), int(y2)),
+                        (255, 0, 0),
+                        2,
+                    )
+                    cv2.putText(
+                        color_image,
+                        f'{label}: {conf:.2f}',
+                        (int(x1), int(y1) - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.9,
+                        (255, 0, 0),
+                        2,
+                    )
 
         # cv2.imshow("Image Detection", color_image)
 
         # Convert OpenCV image back to ROS Image message
         model_output_msg = self.bridge.cv2_to_imgmsg(color_image, encoding='bgr8')
-        
+
         # Publish the model output
         self.model_pub.publish(model_output_msg)
-    
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             rclpy.shutdown()
             cv2.destroyAllWindows()
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -124,5 +158,6 @@ def main(args=None):
         detector.destroy_node()
         rclpy.shutdown()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
