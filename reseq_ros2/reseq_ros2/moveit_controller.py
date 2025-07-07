3897a2bb61d89c12fc289abe6703cceedf146c9c
Lorenzo Ciocca <103076648+lorixhack@users.noreply.github.com>
2025-07-07 18:30:27 +0200

    feat: publish mk2 arm topics reading moveit servo output

--- reseq_ros2/reseq_ros2/moveit_controller.py
+++ reseq_ros2/reseq_ros2/moveit_controller.py
@@ -3,6 +3,8 @@ import traceback
 import rclpy
 from geometry_msgs.msg import TwistStamped, Vector3
 from rclpy.node import Node
+from std_msgs.msg import Float32, Float32MultiArray
+from trajectory_msgs.msg import JointTrajectory
 from std_srvs.srv import SetBool, Trigger
 
 """
@@ -13,12 +15,14 @@ It receives a Twist message from Scaler which contains linear and angular veloci
 of mk2 arm. It prepares the TwistStamped message for Moveit Servo.
 """
 
-
 class MoveitController(Node):
    
     def __init__(self):
         super().__init__('moveit_controller')
-
+        # Declaring parameters and getting values
+        self.arm_module_address = (
+            self.declare_parameter('arm_module_address', 0).get_parameter_value().integer_value
+        )
 
         # Service that manages linear/angular velocities 
         self.linear_vel_enabled = True
@@ -31,8 +35,42 @@ class MoveitController(Node):
         self.speed_pub = self.create_publisher(TwistStamped, '/servo_node/delta_twist_cmds', 10)
 
         self.activate_servo()
+
+        self.create_subscription(
+            JointTrajectory,
+            '/mk2_arm_controller/joint_trajectory',
+            self.send_joint_trajectory,
+            10,
+        )
         
-        self.get_logger().info('Moveit controller node started')
+        self.pubs = {}
+        addr = self.arm_module_address
+        self.pubs['pitch_joint_1_2'] = self.create_publisher(
+            Float32MultiArray,
+            f'reseq/module{addr}/mk2_arm/pitch_joint_1_2/setpoint',
+            10,
+        )
+        self.pubs['pitch_joint_3'] = self.create_publisher(
+            Float32,
+            f'reseq/module{addr}/mk2_arm/pitch_joint_3/setpoint',
+            10,
+        )
+        self.pubs['roll_joint_4'] = self.create_publisher(
+            Float32,
+            f'reseq/module{addr}/mk2_arm/roll_joint_4/setpoint',
+            10,
+        )
+        self.pubs['pitch_joint_5'] = self.create_publisher(
+            Float32,
+            f'reseq/module{addr}/mk2_arm/pitch_joint_5/setpoint',
+            10,
+        )
+        self.pubs['roll_joint_6'] = self.create_publisher(
+            Float32,
+            f'reseq/module{addr}/mk2_arm/roll_joint_6/setpoint',
+            10,
+        )
+        self.get_logger().info('Node Moveit Controller started successfully')
 
     def handle_velocities(self, msg: Vector3):
         servo_msg = TwistStamped()
@@ -63,7 +101,28 @@ class MoveitController(Node):
         
         self.req = Trigger.Request()
         self.activate_service.call_async(self.req)
-        
+    
+    def send_joint_trajectory(self, msg: JointTrajectory):
+        update_diff = True
+        names = msg.joint_names
+        positions = msg.points[len(msg.points) - 1].positions
+
+        for i in range(0, len(names)):
+            if (names[i] == "pitch_joint_1" or names[i] == "roll_joint_2"):
+                if update_diff:
+                    i0 = self.find_index_by_name(names, "pitch_joint_1")
+                    i1 = self.find_index_by_name(names, "roll_joint_2")
+                    update_diff = False
+                    self.pubs["pitch_joint_1_2"].publish(Float32MultiArray(data = [positions[i0], positions[i1]]))
+            else:
+                self.pubs[names[i]].publish(Float32(data = positions[i]))
+
+    def find_index_by_name(self, joints, joint_name):
+        for i in range(0, len(joints)):
+            if joints[i] == joint_name:
+                return i
+
+
 def main(args=None):
     rclpy.init(args=args)
     try:
