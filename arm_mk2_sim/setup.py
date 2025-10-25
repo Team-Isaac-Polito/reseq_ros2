b5ca8247c5056a87409ebf74ad8cbae9419e690f
grigiot <175511406+grigiot@users.noreply.github.com>
2025-10-25 02:21:05 +0200

    feat(arm-gz) Arm spawned in gazebo

--- arm_mk2_sim/setup.py
+++ arm_mk2_sim/setup.py
@@ -1,3 +1,4 @@
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'arm_mk2_sim'
@@ -7,6 +8,8 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
+        (f'share/{package_name}/launch', glob('launch/*launch.py')),
+        (f'share/{package_name}/worlds', glob('worlds/*.world')),
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
