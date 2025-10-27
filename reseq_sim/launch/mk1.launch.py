677723a07fa8069f2ea0f680cda26fe545c450c0
grigiot <175511406+grigiot@users.noreply.github.com>
2025-10-27 16:38:39 +0100

    chore(sim): Move mk1 sim launch in reseq_sim

--- reseq_sim/launch/mk1.launch.py
+++ reseq_sim/launch/mk1.launch.py
@@ -2,19 +2,24 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    package_name = 'reseq_ros2'
+    mk1_package_name = 'reseq_ros2'
+    package_name = 'reseq_sim'
 
     # Include the standard launch file which contain the robot_state_publisher node
     rsp = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            [os.path.join(get_package_share_directory(package_name), 'launch', 'reseq_launch.py')]
+            [
+                os.path.join(
+                    get_package_share_directory(mk1_package_name), 'launch', 'reseq_launch.py'
+                )
+            ]
         ),
         launch_arguments={
             'use_sim_time': 'true',
