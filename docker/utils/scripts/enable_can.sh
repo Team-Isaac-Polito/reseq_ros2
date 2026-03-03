9ee6d4728eca2110410308123d0a2b741e789d40
Yunus-Selim <yunusselimaytekin@gmail.com>
2026-03-03 07:16:12 +0100

    feat: update Dockerfile and enable_can script for additional ROS packages and corrections

--- docker/utils/scripts/enable_can.sh
+++ docker/utils/scripts/enable_can.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Enable CAN interface on Jetson Orin Nano
-# Pin muxing is required to route CAN signals to the 40-pin header
+# Pin muxing is required to route CAN signals to the J17 connector
+# Values per NVIDIA CAN docs: https://docs.nvidia.com/jetson/archives/r36.4.3/DeveloperGuide/HR/ControllerAreaNetworkCan.html
 
-busybox devmem 0x0c303010 w 0x400
-busybox devmem 0x0c303018 w 0x458
+busybox devmem 0x0c303010 w 0xc400
+busybox devmem 0x0c303018 w 0xc458
 
 modprobe can
 modprobe can_raw
